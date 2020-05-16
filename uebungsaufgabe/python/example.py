7f7f05f5b87da7dabaadad55b5f7a55ca5653f8f
Moritz Marquardt <git@momar.de>
2020-05-16 15:44:17 +0200

    Randomize target sink

--- uebungsaufgabe/python/example.py
+++ uebungsaufgabe/python/example.py
@@ -5,6 +5,7 @@ from CoppeliaSimConnector import CoppeliaSimConnector
 from IIwaKinematics import IIwaKinematics
 import PyKDL
 import numpy as np
+import random
 
 import rospy
 from std_msgs.msg import Bool
@@ -32,6 +33,7 @@ gripper = rospy.Publisher('/gripperClosing', Bool, queue_size=2, latch=False)
 
 rot = PyKDL.Rotation()
 rot.DoRotX(-np.pi)
+# rot.DoRotY(-0.2 * np.pi) # Demonstration for jumping rotation
 
 while True:
     kin.ptp(PyKDL.Frame(rot, PyKDL.Vector(0.25, -0.48, 0.19)), 1.0)
@@ -45,12 +47,14 @@ while True:
 
     # speed is not working with lin() ?!
 
-    # Standalone sink
-    kin.ptp(PyKDL.Frame(rot, PyKDL.Vector(0.25, -0.48, 0.225)), 0.1)
-    kin.ptp(PyKDL.Frame(rot, PyKDL.Vector(0.5, 0.45, 0.25)), 0.3)
-    # Sink at the end of the conveyor
-    #kin.ptp(PyKDL.Frame(rot, PyKDL.Vector(0.25, -0.48, 0.225)), 0.8)
-    #kin.lin(PyKDL.Frame(rot, PyKDL.Vector(0.0, -0.48, 0.225)), 1.0)
+    if random.randint(0, 2) > 0:
+        # Standalone sink
+        kin.ptp(PyKDL.Frame(rot, PyKDL.Vector(0.25, -0.48, 0.225)), 1.0)
+        kin.ptp(PyKDL.Frame(rot, PyKDL.Vector(0.5, 0.45, 0.225)), 0.75)
+    else:
+        # Sink at the end of the conveyor
+        kin.ptp(PyKDL.Frame(rot, PyKDL.Vector(0.25, -0.48, 0.225)), 1.0)
+        kin.lin(PyKDL.Frame(rot, PyKDL.Vector(0.0, -0.48, 0.225)), 1.0)
 
     gripper.publish(False)
 
